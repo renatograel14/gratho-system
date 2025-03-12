c59e534cb9986ecc529b6c46bcc0f02ca429ef22
Renato Grael Teixeira <renato.grael@gmail.com>
2025-03-12 00:02:16 -0300

    Fix build and quality

--- src/characters/AttributeBoost.cpp
+++ src/characters/AttributeBoost.cpp
@@ -6,21 +6,24 @@ namespace characters
     AttributeBoost::AttributeBoost(
         std::string source,
         std::map<characters::EnumAttributes, bool> boost,
-        std::map<characters::EnumAttributes, bool> flaw
-    ) : source(source), boost(boost), flaw(flaw) {}
+        std::map<characters::EnumAttributes, bool> flaw) : source(source), boost(boost), flaw(flaw) {}
 
     int AttributeBoost::CountAttributeInBoost(characters::EnumAttributes attr) const
     {
 
         int count = 0;
-        for(const auto& pair : boost) {
-            if(pair.first == attr && pair.second) {
+        for (const auto &pair : boost)
+        {
+            if (pair.first == attr && pair.second)
+            {
                 count++;
             }
         }
 
-        for(const auto& pair : flaw) {
-            if(pair.first == attr && pair.second) {
+        for (const auto &pair : flaw)
+        {
+            if (pair.first == attr && pair.second)
+            {
                 count--;
             }
         }
